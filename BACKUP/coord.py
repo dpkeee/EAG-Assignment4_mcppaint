3f4ed2ac71cf32e7ae549ab950520460ed685c09
dpkeee <itzdeepika@gmail.com>
2025-04-06 03:08:04 -0700

    working code

--- BACKUP/coord.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-try:
-    while True:
-        x, y = pyautogui.position()
-        print(f"Mouse Position: ({x}, {y})", end="\r")
-except Exception as e:
-    print("Tracking stopped.")
